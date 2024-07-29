821aff396ee381f9e139afd9c5b8c7c8468be11b
Gavin Costin <villa.straylite@proton.me>
2024-07-29 16:23:01 +0800

    fix message name in test class

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,20 +1,14 @@
 from unittest import TestCase
 
-from messages import checkMsg, getMsgLOGON, getMsgNewOrder, getMsgRFQ, getMsgCancel
+from messages import checkMsg, getMsgLogon, getMsgNewOrder, getMsgRFQ, getMsgCancel
 
 class DummyTest(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
-
-    def test_always_fails(self):
-        self.assertFalse(False)
-
     def test_checkMsg(self):
         msg = checkMsg(b"msg", None, None)
         assert msg is not None
 
     def test_getMsgLOGON(self):
-        assert getMsgLOGON is not None
+        assert getMsgLogon is not None
 
     def test_getMsgNewOrder(self):
         assert getMsgNewOrder is not None
